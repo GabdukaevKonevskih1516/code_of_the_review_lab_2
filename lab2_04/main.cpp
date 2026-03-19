c9674d287b1ebeaed28b47569e034470208e6cdc
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-19 23:35:24 +0500

    main.cpp

--- lab2_04/main.cpp
+++ lab2_04/main.cpp
@@ -1,1 +1,27 @@
+#include "Header.h"
 
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    FDoubleLinkedList MyList;
+
+    std::cout << "=== Работа с двусвязным списком ===" << std::endl;
+    std::cout << "Введите количество уникальных чисел: ";
+    int N;
+    if (!(std::cin >> N) || N <= 0) return 0;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int Value;
+        std::cout << "Элемент [" << i + 1 << "]: ";
+        std::cin >> Value;
+        MyList.Append(Value);
+    }
+
+    std::cout << "\nВаш список: ";
+    MyList.Print();
+
+    MyList.DisplayElementsBetweenMinMax();
+
+    return 0;
+}

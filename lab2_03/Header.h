e93a30f5d26b07c2f7c5f517df372afc7f06ac53
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-19 23:34:46 +0500

    Header.h

--- lab2_03/Header.h
+++ lab2_03/Header.h
@@ -1,1 +1,40 @@
+#pragma once
 
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// Структура узла односвязного списка 
+struct TNode
+{
+    int Data;
+    TNode* Next;
+
+    TNode(int InValue) : Data(InValue), Next(nullptr) {}
+};
+
+// Класс упорядоченного связного списка 
+class FOrderedList
+{
+public:
+    FOrderedList();
+    ~FOrderedList();
+
+    // Вставка элемента с сохранением порядка убывания 
+        void InsertSorted(int InValue);
+
+    // Удаление первого вхождения значения 
+        void Remove(int InValue);
+
+    // Вывод списка в консоль 
+        void Print() const;
+
+    // Полная очистка списка 
+        void Clear();
+
+    // Загрузка данных из текстового файла 
+    bool LoadFromFile(const std::string& InFilename);
+
+private:
+    TNode* Head;
+};

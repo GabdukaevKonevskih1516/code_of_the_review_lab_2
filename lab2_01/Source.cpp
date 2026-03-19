e2aa632290fd2331906bfafb78af9ede1a92a4ad
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-19 23:32:09 +0500

    Source.cpp

--- lab2_01/Source.cpp
+++ lab2_01/Source.cpp
@@ -1,1 +1,41 @@
+#pragma once
 
+#include <iostream>
+
+// Структура узла стека согласно условию задачи 
+struct TNode
+{
+    int Data;
+    TNode* Next;
+
+    TNode(int InValue) : Data(InValue), Next(nullptr) {}
+};
+
+// Класс стека с дружественным интерфейсом и методами управления 
+class FStack
+{
+public:
+    FStack();
+    ~FStack();
+
+    // Добавить элемент в вершину(Push) 
+        void Push(int InData);
+
+    // Удалить элемент из вершины(Pop) 
+        void Pop();
+
+    // Вывод всех элементов в консоль 
+        void Print() const;
+
+    // Очистка всей памяти стека 
+        void Clear();
+
+    // Получить указатель на вершину 
+        TNode * GetTop() const { return Top; }
+
+private:
+    TNode* Top;
+};
+
+// Функция для выполнения конкретного задания подгруппы 
+void ExecuteTaskStep(FStack & InStack, int InD);

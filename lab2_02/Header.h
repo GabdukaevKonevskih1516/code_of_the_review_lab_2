dced970cf7a02f8653c21342e9620339bd659895
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-19 23:33:50 +0500

    Header.h

--- lab2_02/Header.h
+++ lab2_02/Header.h
@@ -1,1 +1,74 @@
+#include "Header.h"
 
+FQueue::FQueue() : Head(nullptr), Tail(nullptr) {}
+
+FQueue::~FQueue()
+{
+    Clear();
+}
+
+void FQueue::Enqueue(int InData)
+{
+    // FIX_ME: Имена Uzel заменены на TNode 
+    TNode* NewNode = new TNode(InData);
+
+    if (!Head)
+    {
+        Head = NewNode;
+        Tail = NewNode;
+    }
+    else
+    {
+        Tail->Next = NewNode;
+        Tail = NewNode;
+    }
+}
+
+void FQueue::Dequeue()
+{
+    if (!Head)
+    {
+        std::cout << "Ошибка: Очередь пуста!" << std::endl;
+        return;
+    }
+
+    TNode* Temp = Head;
+    Head = Head->Next;
+    delete Temp;
+
+    if (!Head)
+    {
+        Tail = nullptr;
+    }
+}
+
+void FQueue::Print() const
+{
+    TNode* Current = Head;
+    while (Current)
+    {
+        std::cout << Current->Data << " ";
+        Current = Current->Next;
+    }
+    std::cout << std::endl;
+}
+
+void FQueue::DisplayPointers() const
+{
+    // FIX_ME: Явное приведение к void* для корректного отображения HEX-адресов
+    std::cout << "Указатель Head: " << (void*)Head;
+    if (Head) std::cout << " (Data: " << Head->Data << ")";
+    std::cout << std::endl;
+
+    std::cout << "Указатель Tail: " << (void*)Tail;
+    if (Tail) std::cout << " (Data: " << Tail->Data << ")";
+    std::cout << std::endl;
+}
+
+void FQueue::Clear()
+{
+    while (Head)
+    {
+        Dequeue();
+    }
+}

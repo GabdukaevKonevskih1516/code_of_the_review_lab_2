3d91cb116fd5a6f94eadd7ca3258f829805d8919
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-19 23:33:32 +0500

    Source.cpp

--- lab2_02/Source.cpp
+++ lab2_02/Source.cpp
@@ -1,1 +1,40 @@
+#pragma once
 
+#include <iostream>
+
+struct TNode
+{
+    int Data;
+    TNode* Next;
+
+    TNode(int InValue) : Data(InValue), Next(nullptr) {}
+};
+
+// Класс очереди (Queue) на базе связанных узлов TNode.
+// Реализует методы добавления, удаления и вывода адресов Head / Tail.
+
+class FQueue
+{
+public:
+    FQueue();
+    ~FQueue();
+
+    // Добавить элемент в конец очереди(Tail) 
+        void Enqueue(int InData);
+
+    // Удалить элемент из начала очереди(Head) 
+        void Dequeue();
+
+    // Вывод всех элементов в консоль 
+        void Print() const;
+
+    // Вывод адресов начала и конца очереди 
+        void DisplayPointers() const;
+
+    // Полная очистка очереди 
+    void Clear();
+
+private:
+    TNode* Head; // Начало очереди
+    TNode* Tail; // Конец очереди
+};
